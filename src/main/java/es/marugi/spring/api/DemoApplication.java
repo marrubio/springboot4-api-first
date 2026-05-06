d7a3c9f2955341a0fb64d9cb70402ec124a05b2d
Mario Rubio <marugi@gmail.com>
2026-05-06 17:11:17 +0200

    Change main method to public access modifier

--- src/main/java/es/marugi/spring/api/DemoApplication.java
+++ src/main/java/es/marugi/spring/api/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
